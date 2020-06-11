10bbf0a13978b410db76a3c777e47689a3f05506
e-shimada <e-shimada@doti04-PC>
2020-06-11 09:45:11 +0900

    ラジオボタン AM・PM・の設定。idの追加。

--- src/main/resources/static/MessageMemo.js
+++ src/main/resources/static/MessageMemo.js
@@ -10,30 +10,57 @@ function setDate(){
     document.getElementById("today_day").value=nowDay;
 }
 
-// 受電日時
+// システム日時
+//function setTime() {
+//	let today = new Date();
+//	let nowHour = today.getHours();
+//	let nowMinute = today.getMinutes();
+//	
+//	if(nowHour >= 0 && nowHour <= 11) {
+//		if(nowHour == 0) {
+//			document.getElementById("hour").value = nowHour + 12;
+//		} else {
+//			document.getElementById("hour").value = nowHour;
+//		}
+//		let check_am = document.getElementById("radio_1");
+//		check_am.checked = true;
+//	} else {
+//		if(nowHour >= 13) {
+//			document.getElementById("hour").value = nowHour - 12;
+//		} else {
+//			document.getElementById("hour").value = nowHour;
+//		}
+//		let check_pm = document.getElementById("radio_2");
+//		check_pm.checked = true;
+//	}
+//	
+//	document.getElementById("minute").value = nowMinute;
+//}
+
 function setTime() {
-    let today = new Date();
+	let today = new Date();
     let hours = today.getHours();
     let minutes = today.getMinutes(); 
 
-    document.getElementById("hour").value = hours;
-    document.getElementById("minute").value = minutes;
+   document.getElementById("hour").value = hours;
+   document.getElementById("minute").value = minutes;
 
-    // 13時以降の場合
-    if (hours > 12) {
-        document.getElementById("hour").value = hours - 12;
-    } else if (hours == 0) {
+   // 13時以降の場合
+   if (hours > 12) {
+       document.getElementById("hour").value = hours - 12;
+   } else if (hours == 0) {
         document.getElementById("hour").value = 12;
-    } else {
+   } else {
         document.getElementById("hour").value = hours;
-    }
+   }
 
     // AM・PMのチェック
     if (hours >= 0 && hours <= 11) {
         document.getElementById("am").checked = true;
     } else if(hours >= 12 && hours <= 23) {
-        document.getElementById("pm").checked = true;
+       document.getElementById("pm").checked = true;
     }
+    
 }
 
 //チェックボックス
@@ -75,16 +102,39 @@ function check_readonly(){
       t.readOnly = true;
       target.value ="";
   }
-
 }
 
-////document.getElementById("check3"").onclick = function() {
-//    let target = document.getElementById("target");
-//        if (document.getElementById("check3").checked) {
-//            target.readOnly = false;
-//        } else {
-//            target.readOnly = true;
-//        }
-//}
+
+
+//入力チェック
+// チェックボックスのいずれかにチェックされていない場合は、メッセージ（「対応のいづれかにチェックしてください」など）を表示すること
+//「伝言あります」にチェックが入っている場合にのみ、メモは必須になります
+function formCheck(){  
+	let flag = 0;
+    if( document.form.check1.checked == false && document.form.check2.checked == false && document.form.check3.checked == false){
+        flag = 1;
+        document.getElementById( 'notice-input-text-1' ).style.display = "block";
+       
+    }else{
+        document.getElementById( 'notice-input-text-1' ).style.display = "none";
+        
+    }
+    
+    if( document.form.check3.checked == true && document.form.memo.value == ""){
+    	flag = 1;
+        document.getElementById( 'notice-input-text-2' ).style.display = "block";
+     
+    }else{
+        document.getElementById( 'notice-input-text-2' ).style.display = "none";
+     
+    }
+    
+    if( flag ){ // 入力必須項目に未入力があった場合
+        return false; // 送信中止
+    }else{ // 入力必須項目が全て入力済みだった場合
+        return true; // 送信実行
+    }
+
+ }
 
 
